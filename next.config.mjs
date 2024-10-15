75933d06cb43f757dd6e2bb4ce24243dfc8701d6
Gean Maidana <gean-2010@live.com>
2024-10-14 22:31:06 -0500

    :wq
    :wq
    
    :wq
    
    :wq
    
    :wq

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
-
-
+const nextConfig = {
+    output: 'export',
+    basePath: '',
+    trailingSlash: true,
+  };
+  
+  export default nextConfig;
+  
